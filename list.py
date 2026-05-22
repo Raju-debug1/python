dad0307c4790887fca48ff905c089116ffc6e4b2
Raju-debug1 <radarpaudel1@gmail.com>
2026-05-22 07:38:16 +0545

    hlo

--- list.py
+++ list.py
@@ -9,4 +9,14 @@ print(numbers[-1])
 print(numbers[-2])
 print(numbers[-3])
 print(numbers[-4])
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+print(fruits[0])  # Output: apple
+print(fruits[1])  # Output: banana
+print(fruits[2])  # Output: cherry
+print(fruits[3])  # Output: date
+print(fruits[4])  # Output: elderberry
 
+numbers = [10,20,30,40,50]
+print(numbers[1:4])
+print(numbers[::2])
+print(numbers[::-1])
