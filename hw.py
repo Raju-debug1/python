1bbe769f6427b8ba85d339b1226052810830099d
Raju-debug1 <radarpaudel1@gmail.com>
2026-05-27 07:20:32 +0545

    hw

---
+++ hw.py
@@ -0,0 +1,20 @@
+#print the first and last fruit using indexing.
+
+fruits = ["apple", "banana", "cherry", "mango", "grape"]
+print(fruits[0])
+print(fruits[-1])
+
+#print a sublist from index 1 to 3 using slicing.
+
+fruits = ["apple", "banana", "cherry", "mango", "grape"]
+print(fruits[1:4])
+
+#print every second fruit from the list.
+
+fruits = ["apple", "banana", "cherry", "mango", "grape"]
+print(fruits[::2])
+
+#print the reversed list using slicing.
+
+fruits = ["apple", "banana", "cherry", "mango", "grape"]
+print(fruits[::-1])
