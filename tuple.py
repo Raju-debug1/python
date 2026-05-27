3647af3742e3e9aabc3a0ea2c104f86649ad52e6
Raju-debug1 <radarpaudel1@gmail.com>
2026-05-27 07:43:23 +0545

    tuple

--- tuple.py
+++ tuple.py
@@ -2,3 +2,22 @@
 my_list = [1, 2, 3, 4, 5]
 my_tuple = tuple(my_list)
 print(my_tuple)
+
+# to convert tuple into list
+my_tuple = (1, 2, 3, 4, 5) 
+my_list = list(my_tuple)
+print(my_list)  
+
+
+my_string = "Hello, World!"
+my_tuple = tuple(my_string)
+print(my_tuple)
+
+my_string = "Hello, World!"
+my_list = list(my_string)
+print(my_list)
+
+# to convert tuple into string
+my_tuple = ('H', 'e', 'l', 'l', 'o')
+my_string = ''.join(my_tuple)
+print(my_string)
