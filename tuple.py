033ad12d89022cdcbd00b280b933073f48082e7b
Raju-debug1 <radarpaudel1@gmail.com>
2026-05-27 07:54:25 +0545

    tuple

--- tuple.py
+++ tuple.py
@@ -21,3 +21,20 @@ print(my_list)
 my_tuple = ('H', 'e', 'l', 'l', 'o')
 my_string = ''.join(my_tuple)
 print(my_string)
+
+# to convert dict into tuple
+my_dict = {"name": "Alice", "age": 30, "city": "New York"}
+my_tuple = tuple(my_dict.items())
+print(my_tuple)
+
+
+# to convert tuple into dict
+my_tuple = (("name", "Alice"), ("age", 30), ("city", "New York"))
+my_dict = dict(my_tuple)
+print(my_dict)
+
+
+# to convert dict into list
+my_dict = {"name": "Alice", "age": "30", "city": "New York"}
+my_list = list(my_dict.items())
+print(my_list)
